6746bf76117f3605a2194c94545178d4c235bba9
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-06-11 01:18:04 +0200

    ✨ feat(mongoose): connect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 
 @Module({
   imports: [
@@ -13,7 +16,17 @@ import { LoggerModule } from 'nestjs-pino';
           translateTime: 'UTC:mm/dd/yyyy, h:MM:ss TT Z',
         }
       }
-    })
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (env: ConfigService) => ({
+        uri: process.env.MONGO_URL || `mongodb://${env.get('db_user')}:${env.get('db_pass')}@${env.get('db_uri')}:${env.get('db_port')}/${env.get('db_name')}`,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    }),
+    ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
