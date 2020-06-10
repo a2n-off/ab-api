275639b1d8e9ff85e10eda6497da46357ba7c466
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-06-11 00:04:09 +0200

    🔥  feat(logger): clean up

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Logger } from 'nestjs-pino/dist';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly logger: Logger) {}
-
   getHello(): string {
-    this.logger.log('log', AppService.name);
     return 'Hello World!';
   }
 }
